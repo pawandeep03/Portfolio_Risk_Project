691e4e116a3eab28044866b2004c6cc3a2c4da22
pawandeep03 <pavndeep0803@gmail.com>
2026-02-01 14:20:49 +0530

    Delete SQL directory

--- SQL/data_cleaning.sql
+++
@@ -1,9 +0,0 @@
-
--- Data Cleaning Queries
-
--- Remove NULL values
-DELETE FROM customers WHERE customer_id IS NULL;
-DELETE FROM orders WHERE order_amount IS NULL;
-
--- Remove negative revenue (returns)
-DELETE FROM orders WHERE order_amount < 0;
